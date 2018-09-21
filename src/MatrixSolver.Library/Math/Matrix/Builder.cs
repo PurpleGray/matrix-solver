453c79909036832c79b3224eb1e677a373962163
PurpleGray <ValterBishop@gmail.com>
2018-09-21 19:45:47 +0300

    Partially implemented Builder for integer type

--- src/MatrixSolver.Library/Math/Matrix/Builder.cs
+++ src/MatrixSolver.Library/Math/Matrix/Builder.cs
@@ -8,13 +8,38 @@ namespace MatrixSolver.Library.Math.LinearAlgebra.Matrix
     {
         public abstract T Zero { get; }
 
-        public abstract Matrix<T> FromArray(T[,] arr, int rowNumer, int colNumber);
+        public abstract Matrix<T> FromArray(T[,] arr, int rowNumber, int colNumber);
 
         public abstract Matrix<T> FromFormattedString(string rawString);
 
         public abstract Matrix<T> FromDimensions(int rowCount, int columnCount);
 
         public abstract Matrix<T> SameAs(Matrix<T> matrix);
+    }
+
+    public class MatrixBuilder : MatrixBuilder<int>
+    {
+        public override int Zero { get; } = 0;
+        
+        public override Matrix<int> FromArray(int[,] arr, int rowNumber, int colNumber)
+        {
+            return new Integer.Matrix(new MatrixDataContainer(rowNumber, colNumber, arr));
+        }
+
+        public override Matrix<int> FromFormattedString(string rawString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Matrix<int> FromDimensions(int rowCount, int columnCount)
+        {
+            return new Integer.Matrix(new MatrixDataContainer(rowCount, columnCount));
+        }
+
+        public override Matrix<int> SameAs(Matrix<int> matrix)
+        {
+            return new Integer.Matrix(new MatrixDataContainer(matrix.RowCount, matrix.ColumnCount));
+        }
     }
     
     internal static class BuilderInstance<T> where T : struct, IEquatable<T>, IFormattable
@@ -29,7 +54,7 @@ namespace MatrixSolver.Library.Math.LinearAlgebra.Matrix
                 throw new NotImplementedException(); // TODO: implement integer builder
             }
             
-            // Someday need to add other types
+            // In further releases implement other types
             throw new NotSupportedException(string.Format(
                 "Matrices of type '{0}' are not supported. Only Integer is supported at this point.",
                 typeof(T).Name));
